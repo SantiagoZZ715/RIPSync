8e00d27b7dfa5bf8de6a5b10743e01f6f83d97ae
SantiagoZZ715 <santiagoqw1234@gmail.com>
2025-02-26 13:46:01 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,14 +15,14 @@ logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 async def start(update: Update, context: CallbackContext) -> None:
     """Mensaje de bienvenida"""
-    await update.message.reply_text("¡Hola! Envíame un código CUPS, una descripción y el tipo de atención (AP o AC) en el siguiente formato \n\n `CUPS;Descripción;Tipo de Atención`")
+    await update.message.reply_text("¡Hola! Envíame un código CUPS, una descripción y el tipo de atención (AP o AC) en el siguiente formato \n\n `CUPS,Descripción,Tipo de Atención`")
 
 async def validate(update: Update, context: CallbackContext) -> None:
     """Procesa el mensaje del usuario"""
-    mensaje = update.message.text.split(";")  # Esperamos el mensaje en formato "cups;descripcion;tipo"
+    mensaje = update.message.text.split(",")  # Esperamos el mensaje en formato "cups;descripcion;tipo"
     
     if len(mensaje) != 3:
-        await update.message.reply_text("⚠️ Formato incorrecto. Envíame el mensaje en este formato:\n\n`CUPS;Descripción;Tipo de Atención`")
+        await update.message.reply_text("⚠️ Formato incorrecto. Envíame el mensaje en este formato:\n\n`CUPS,Descripción,Tipo de Atención`")
         return
     
     cups_ingresado, descripcion, tipo_atencion = mensaje[0].strip(), mensaje[1].strip(), mensaje[2].strip()
